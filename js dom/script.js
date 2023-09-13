69a791e19c422842e9a8bd9ce8bea42d539458b0
Zeldrise <fandiekapratama.0612@gmail.com>
2023-09-14 06:26:49 +0700

    menambah file

---
+++ js dom/script.js
@@ -0,0 +1,30 @@
+const judul = document.getElementById("judul");
+judul.style.color = "navy";
+judul.style.backgroundColor = "yellow";
+judul.innerHTML = "samlekom";
+
+// const p = document.getElementsByTagName("p");
+// for (let i = 0; i < p.length; i++) p[i].style.backgroundColor = "lightgreen";
+
+const h1 = document.getElementsByTagName("h1")[0];
+h1.style.fontSize = "50px";
+
+const p1 = document.getElementsByClassName("p1")[0];
+p1.innerHTML = "kata bu ber ini dirubah pake javascript";
+
+// const p4 = document.querySelector("#b p");
+// p4.style.color = "blue";
+// p4.style.fontSize = "25px";
+
+const li2 = document.querySelector("li:nth-child(2)");
+li2.style.backgroundColor = "lightcoral";
+
+const p = document.querySelectorAll("p");
+for (let i = 0; i < p.length; i++) p[i].style.backgroundColor = "pinks";
+
+// const p4 = document.querySelectorAll("p")[3];
+// p4.style.backgroundColor = "yellow";
+
+const sectionb = document.getElementById("b");
+const p4 = sectionb.querySelector("p");
+p4.style.backgroundColor = "green";
